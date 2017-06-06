ff2f946411e49a701fad6d67b558c09e17732c2f
Meghabh <meghahavaragi@gmail.com>
2017-06-06 11:32:16 +0530

    initial commit

---
+++ src/Test/LearnPatchTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+import static junit.framework.TestCase.fail;
+
+/**
+ * Created by meghah on 6/6/17.
+ */
+public class LearnPatchTest {
+
+    @Test
+    public void shouldFail(){
+        fail("should fail");
+    }
+}
